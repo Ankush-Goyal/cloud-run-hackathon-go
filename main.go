5ca13d503ecf74c8a3d58e23ae4bd2c3da05eae4
Ankush Goyal <ankush.goyal@VER-BLR-LT2820.local>
2022-11-18 14:19:53 +0530

    Personal commit

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 )
 
+//var count int = 0
+
 func main() {
 	port := "8080"
 	if v := os.Getenv("PORT"); v != "" {
@@ -41,12 +43,127 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, resp)
 }
 
-func play(input ArenaUpdate) (response string) {
+func play(input ArenaUpdate) string {
 	log.Printf("IN: %#v", input)
 
 	commands := []string{"F", "R", "L", "T"}
-	rand := rand2.Intn(4)
-	
-	// TODO add your implementation here to replace the random response
-	return commands[rand]
+	// var rand int = 3
+	// if count == 3 {
+	// 	rand = rand2.Intn(4)
+	// }
+	// //TODO add your implementation here to replace the random response
+	// count++
+	// count = count % 4
+
+	//return commands[rand]
+
+	res := process(input)
+	if res == "" {
+		log.Println("Status", "got empty sending random")
+		res = commands[rand2.Intn(4)]
+	}
+	log.Println("Status", "res:", res)
+
+	return res
+}
+
+func increment(dirMap map[string]int, dir string) {
+	val, ok := dirMap[dir]
+	if !ok {
+		dirMap[dir] = 1
+	}
+	dirMap[dir] = val + 1
+}
+
+func process(input ArenaUpdate) string {
+	me := input.Arena.State[input.Links.Self.Href]
+	if me.WasHit {
+		log.Println("Status", "got hit from someone")
+		return ""
+	}
+	dirMap := make(map[string]int, 4)
+	diffX, diffY := 0, 0
+	switch me.Direction {
+	case "N":
+		for _, pl := range input.Arena.State {
+			diffX, diffY = me.X-pl.X, me.Y-pl.Y
+
+			if diffX == 0 && diffY > 0 && diffY <= 3 {
+				log.Println("Status", "got someone in current direction")
+				return "T"
+			}
+			if diffX > 0 && diffX <= 3 {
+				increment(dirMap, "R")
+			} else if diffX < 0 && diffX >= -3 {
+				increment(dirMap, "L")
+			} else {
+				increment(dirMap, "F")
+			}
+		}
+
+	case "E":
+		for _, pl := range input.Arena.State {
+			diffX, diffY = me.X-pl.X, me.Y-pl.Y
+
+			if diffY == 0 && diffX > 0 && diffX <= 3 {
+				log.Println("Status", "got someone in current direction")
+				return "T"
+			}
+			if diffY > 0 && diffY <= 3 {
+				increment(dirMap, "L")
+			} else if diffY < 0 && diffY >= -3 {
+				increment(dirMap, "R")
+			} else {
+				increment(dirMap, "F")
+			}
+		}
+
+	case "S":
+		for _, pl := range input.Arena.State {
+			diffX, diffY = me.X-pl.X, me.Y-pl.Y
+
+			if diffX == 0 && diffY < 0 && diffY >= -3 {
+				log.Println("Status", "got someone in current direction")
+				return "T"
+			}
+			if diffX > 0 && diffX <= 3 {
+				increment(dirMap, "L")
+			} else if diffX < 0 && diffX >= -3 {
+				increment(dirMap, "R")
+			} else {
+				increment(dirMap, "F")
+			}
+		}
+	case "W":
+		for _, pl := range input.Arena.State {
+			diffX, diffY = me.X-pl.X, me.Y-pl.Y
+
+			if diffY == 0 && diffX < 0 && diffX >= -3 {
+				log.Println("Status", "got someone in current direction")
+				return "T"
+			}
+			if diffY > 0 && diffY <= 3 {
+				increment(dirMap, "L")
+			} else if diffY < 0 && diffY >= -3 {
+				increment(dirMap, "R")
+			} else {
+				increment(dirMap, "F")
+			}
+		}
+	}
+	var (
+		max     int    = 0
+		nextDir string = "U"
+	)
+	for k, v := range dirMap {
+		if v > max {
+			max = v
+			nextDir = k
+		}
+	}
+	if max == 0 && nextDir == "F" {
+		nextDir = "" // will denote go for random
+	}
+	log.Println("Status", dirMap, max, nextDir)
+	return nextDir
 }
